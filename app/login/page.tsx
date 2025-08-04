1377ab04599ebb41db151799120f5b26e61b9224
Hoang Lee <hoanglee26222@gmail.com>
2025-08-04 15:38:19 +0700

    up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
   const handleGoogleSignIn = async () => {
     setIsLoading(true)
     try {
-      window.open(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email%20profile&access_type=offline`)
+      window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email%20profile&access_type=offline`
     } catch (error) {
       toast.error(t("auth.googleConnectionError"))
     } finally {
